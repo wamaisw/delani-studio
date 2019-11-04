9686d47bfaf4d66af48eba4e312e712b64077736
steve.wamai <wamaisw@gmail.com>
2019-11-04 14:22:18 +0300

    added click effect to what we do

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,8 +1,38 @@
-jQuery('form#form1').submit(function (event) {
-  event.preventDefault();
-  var userName = document.getElementById('name').value;
-  alert(userName + ' we have received your message. Thank you for reaching out to us.');
-  jQuery('#contact-form')[0].reset();
-// document.getElementById('contact-form').reset();
-})
-
+$(document).ready(function(){
+  $(".img1").click(function(){
+  $(".info1").show();
+  });
+  $(".img1").click(function(){
+  $(".img1").hide();
+  });
+  $(".info1").click(function(){
+   $(".img1").show();
+});
+ $(".info1").click(function(){
+ $(".info1").hide();
+ });
+ $(".img2").click(function(){
+  $(".info2").show();
+  });
+  $(".img2").click(function(){
+  $(".img2").hide();
+  });
+ $(".info2").click(function(){
+ $(".img2").show();
+   });
+   $(".info2").click(function(){
+    $(".info2").hide();
+   });
+$(".img3").click(function(){
+   $(".info3").show();
+   });
+     $(".img3").click(function(){
+   $(".img3").hide();
+  });
+ $(".info3").click(function(){
+  $(".img3").show();
+ });
+ $(".info3").click(function(){
+ $(".info3").hide();
+ });
+});
